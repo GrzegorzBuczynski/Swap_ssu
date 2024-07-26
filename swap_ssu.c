c01ba9318adb51c290c06a7dbe3f88475518303f
Grzegorz Buczyński <79482958+GrzegorzBuczynski@users.noreply.github.com>
2024-07-26 18:35:24 +0200

    Update swap_ssu.c

--- swap_ssu.c
+++ swap_ssu.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
+
 
 #define SPRITE_WIDTH 2
 #define SPRITE_HEIGHT 2
+#define IMAGE_WIDTH 20
+#define IMAGE_HIGHT 20
 
 typedef struct s_game
 {
@@ -35,23 +39,26 @@ void    put_line(char *data_addr, char *sprite_data)
     int i = 0;
     while (i < SPRITE_WIDTH)
     {
-        if (sprite_data[i] == 0)
-            put_pixel(&data_addr[i], &sprite_data[i]);
+        if (sprite_data[i*4] != 0)
+            put_pixel(&data_addr[i*4], &sprite_data[i*4]);
         i++;
     }
 }
 
 void    put_pixel(char *data_addr, char *color)
 {
-    *data_addr = *color;
+    data_addr[0] = color[0];
+    data_addr[1] = color[1];
+    data_addr[2] = color[2];
+    data_addr[3] = color[3];
 }
 
 int main()
 {
     int x = 0;
     int y = 0;
-    char image[9] = {0};
-    char sprite[4] = {1,1,1,1};
+    char image[IMAGE_WIDTH * IMAGE_HIGHT] = {0};
+    char sprite[SPRITE_WIDTH * SPRITE_HEIGHT] = {1,1,1,1};
     
     t_game *game = malloc(sizeof(t_game));
     if (game == NULL) {
@@ -60,7 +67,7 @@ int main()
     }
     
     game->data_addr = image;
-    game->line_size = 3;
+    game->line_size = IMAGE_WIDTH;
     game->colectable = malloc(sizeof(char*));
     if (game->colectable == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
@@ -72,12 +79,12 @@ int main()
     put_sprite(game, 1, 1, 0);
     
     y = 0;
-    while (y < 3)
+    while (y < IMAGE_HIGHT)
     {
         x = 0;
-        while (x < 3)
+        while (x < IMAGE_WIDTH)
         {
-            printf("%d ", image[y * 3 + x]);
+            printf("%d ", image[y * IMAGE_WIDTH + x]);
             x++;
         }
         printf("\n");
